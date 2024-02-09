f0fd2a6865116a0426351dece973169b17591a7e
Tarasov Maksim <maksimtarasov1981@gmail.com>
2024-02-09 16:56:17 +0200

    add engine

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from sqlalchemy import create_engine
+with open('connection.cfg') as f:
+    s = f.readlines()
+username = (s[0].split(':')[1][0:-1])
+password = (s[1].split(':')[1][0:-1])
+host = (s[2].split(':')[1][0:-1])
 
 engine = create_engine(
-    "postgresql+psycopg2://postgres:1111@localhost/sqlalchemy_tuts",
+    f"postgresql+psycopg2://postgres:test_db@localhost/test_db",
     echo=True, pool_size=6, max_overflow=10, encoding='latin1'
 )
 engine.connect()
